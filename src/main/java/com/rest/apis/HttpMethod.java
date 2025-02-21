d32b777e8fd1a85aadd93a5ffaf40ec379d68d95
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-21 16:09:32 +0530

    [Saurabh] Add. Get Hello messages with parameter of name

--- src/main/java/com/rest/apis/HttpMethod.java
+++ src/main/java/com/rest/apis/HttpMethod.java
@@ -2,6 +2,7 @@ package com.rest.apis;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class HttpMethod {
     @GetMapping("/hello")
     public String fn(){
         return "hello:) {muishi? ,muishi!}";
+    }
+    @GetMapping("/hello/query")
+    public String fn2(@RequestParam String name){
+        return "hello:) "+name+" {muishi? ,muishi!}";
     }
 }
