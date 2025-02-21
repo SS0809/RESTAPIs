c0198769cd1066f5b973e07e8a062f8b7aa74316
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-21 16:15:03 +0530

    [Saurabh] Add. Get Hello messages with path variable of name

--- src/main/java/com/rest/apis/HttpMethod.java
+++ src/main/java/com/rest/apis/HttpMethod.java
@@ -2,6 +2,7 @@ package com.rest.apis;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ public class HttpMethod {
     }
     @GetMapping("/hello/query")
     public String fn2(@RequestParam String name){
+        return "hello:) "+name+" {muishi? ,muishi!}";
+    }
+    @GetMapping("hello/param/{name}")
+    public String fn3(@PathVariable String name){
         return "hello:) "+name+" {muishi? ,muishi!}";
     }
 }
